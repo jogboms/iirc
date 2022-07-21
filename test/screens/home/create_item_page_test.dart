744d5ea73f7019bd0a3f3bcf50eaf2377cd896dc
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2022-07-21 19:32:43 +0200

    Fix broken create item page test

--- test/screens/home/create_item_page_test.dart
+++ test/screens/home/create_item_page_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:iirc/screens.dart';
+import 'package:mocktail/mocktail.dart';
 
 import '../../utils.dart';
 
@@ -8,7 +9,12 @@ void main() {
   group('CreateItemPage', () {
     final Finder createItemPage = find.byType(CreateItemPage);
 
+    tearDown(() => mockRepositories.reset());
+
     testWidgets('smoke test', (WidgetTester tester) async {
+      when(() => mockRepositories.tags.fetch()).thenAnswer((_) async* {});
+      when(() => mockRepositories.items.fetch()).thenAnswer((_) async* {});
+
       await tester.pumpWidget(createApp(home: const CreateItemPage(asModal: false)));
 
       await tester.pump();

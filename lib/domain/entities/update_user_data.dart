517d698706da4c39ceac6fd6a8c27bfb2950cf4f
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2022-07-29 09:20:47 +0200

    Introduce `lastSeenAt` on `UserModel` w/ update repo action

---
+++ lib/domain/entities/update_user_data.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+class UpdateUserData with EquatableMixin {
+  const UpdateUserData({
+    required this.id,
+    required this.lastSeenAt,
+  });
+
+  final String id;
+  final DateTime lastSeenAt;
+
+  @override
+  List<Object?> get props => <Object?>[id, lastSeenAt];
+
+  @override
+  bool? get stringify => true;
+}
--- lib/domain/repositories/users.dart
+++ lib/domain/repositories/users.dart
@@ -1,8 +1,11 @@
+import '../entities/update_user_data.dart';
 import '../models/account.dart';
 import '../models/user.dart';
 
 abstract class UsersRepository {
   Future<String> create(AccountModel account);
+
+  Future<bool> update(UpdateUserData user);
 
   Future<UserModel?> fetch(String uid);
 }
